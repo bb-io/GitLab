0680decb5a06843449f8a99d54a1075d31987a55
bohdanm-bb <bohdan.makarenko@blackbird.io>
2024-04-19 01:56:29 +0300

    File actions

--- Apps.GitLab/Actions/RepositoryActions.cs
+++ Apps.GitLab/Actions/RepositoryActions.cs
@@ -12,8 +12,15 @@ using Blackbird.Applications.SDK.Extensions.FileManagement.Interfaces;
 using Apps.GitHub.Models.Branch.Requests;
 using GitLabApiClient.Models.Projects.Responses;
 using GitLabApiClient.Internal.Paths;
-using GitLabApiClient.Models.Branches.Requests;
 using GitLabApiClient.Models.Projects.Requests;
+using Apps.GitLab;
+using Apps.Gitlab.Models.Commit.Responses;
+using Blackbird.Applications.Sdk.Utils.Models;
+using Blackbird.Applications.Sdk.Utils.Extensions.Files;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using GitLabApiClient.Models.Commits.Requests;
+using Microsoft.Extensions.Options;
 
 namespace Apps.Gitlab.Actions;
 
@@ -35,89 +42,86 @@ public class RepositoryActions : GitLabActions
         return repository;
     }
 
-    //[Action("Get repository file", Description = "Get repository file by path")]
-    //public GetFileResponse GetFile(
-    //    [ActionParameter] GetRepositoryRequest repositoryRequest,
-    //    [ActionParameter] GetOptionalBranchRequest branchRequest,
-    //    [ActionParameter] GetFileRequest getFileRequest)
-    //{
-    //    var repoInfo = GetRepositoryById(repositoryRequest);
-    //    var fileData = string.IsNullOrEmpty(branchRequest.Name) ?
-    //        Client.Repository.Content.GetRawContent(repoInfo.OwnerLogin, repoInfo.Name, getFileRequest.FilePath).Result :
-    //        Client.Repository.Content.GetRawContentByRef(repoInfo.OwnerLogin, repoInfo.Name, getFileRequest.FilePath, branchRequest.Name).Result;
-    //    if(fileData == null)
-    //        throw new ArgumentException($"File does not exist ({getFileRequest.FilePath})");
+    [Action("Get repository file", Description = "Get repository file by path")]
+    public async Task<GetFileResponse> GetFile(
+        [ActionParameter] GetRepositoryRequest repositoryRequest,
+        [ActionParameter] GetOptionalBranchRequest branchRequest,
+        [ActionParameter] GetFileRequest getFileRequest)
+    {
+        var projectId = (ProjectId)int.Parse(repositoryRequest.RepositoryId);
+        var fileData = await Client.Files.GetAsync(projectId, getFileRequest.FilePath, branchRequest.Name ?? null);
+        if (fileData == null)
+            throw new ArgumentException($"File does not exist ({getFileRequest.FilePath})");
 
-    //    var filename = Path.GetFileName(getFileRequest.FilePath);
-    //    if (!MimeTypes.TryGetMimeType(filename, out var mimeType))
-    //        mimeType = MediaTypeNames.Application.Octet;
+        var filename = Path.GetFileName(getFileRequest.FilePath);
+        if (!MimeTypes.TryGetMimeType(filename, out var mimeType))
+            mimeType = MediaTypeNames.Application.Octet;
 
-    //    FileReference file;
-    //    using (var stream = new MemoryStream(fileData))
-    //    {
-    //        file = _fileManagementClient.UploadAsync(stream, mimeType, filename).Result;
-    //    }
-        
-    //    return new GetFileResponse
-    //    {
-    //        FilePath = getFileRequest.FilePath,
-    //        File = file,
-    //        FileExtension = Path.GetExtension(getFileRequest.FilePath)
-    //    };
-    //}
+        FileReference file;
+        using (var stream = new MemoryStream(Convert.FromBase64String(fileData.Content)))
+        {
+            file = _fileManagementClient.UploadAsync(stream, mimeType, filename).Result;
+        }
 
-    //[Action("Get all files in folder", Description = "Get all files in folder")]
-    //public async Task<GetRepositoryFilesFromFilepathsResponse> GetAllFilesInFolder(
-    //    [ActionParameter] GetRepositoryRequest repositoryRequest,
-    //    [ActionParameter] GetOptionalBranchRequest branchRequest,
-    //    [ActionParameter] FolderContentRequest folderContentRequest)
-    //{
-    //    var resultFiles = new List<GithubFile>();
-    //    var content = string.IsNullOrEmpty(branchRequest.Name) ?
-    //        await Client.Repository.Content.GetArchive(long.Parse(repositoryRequest.RepositoryId), ArchiveFormat.Zipball) :
-    //        await Client.Repository.Content.GetArchive(long.Parse(repositoryRequest.RepositoryId), ArchiveFormat.Zipball, branchRequest.Name);
-    //    if(content == null || content.Length == 0) 
-    //    {
-    //        throw new ArgumentException("Repository is empty!");
-    //    }
+        return new GetFileResponse
+        {
+            FilePath = getFileRequest.FilePath,
+            File = file,
+            FileExtension = Path.GetExtension(getFileRequest.FilePath)
+        };
+    }
 
-    //    var filesFromZip = new List<BlackbirdZipEntry>();
-    //    using(var stream = new MemoryStream(content))
-    //    {
-    //        filesFromZip = (await stream.GetFilesFromZip()).ToList();
-    //    }
-    //    foreach (var file in filesFromZip)
-    //    {
-    //        file.Path = file.Path.Substring(file.Path.IndexOf('/') + 1);
-    //        var includeSubFolders = folderContentRequest.IncludeSubfolders.HasValue && folderContentRequest.IncludeSubfolders.Value;
-    //        if (file.FileStream.Length == 0)
-    //        {
-    //            continue;
-    //        }
-    //        else if (!string.IsNullOrEmpty(folderContentRequest.Path))
-    //        {
-    //            if ((includeSubFolders && !file.Path.StartsWith(folderContentRequest.Path)) ||
-    //                (!includeSubFolders && Path.GetDirectoryName(file.Path).TrimStart('\\').Replace('\\', '/') != folderContentRequest.Path.Trim('/')))
-    //            {
-    //                continue;
-    //            }
-    //        }
-    //        else if(!includeSubFolders && !string.IsNullOrEmpty(Path.GetDirectoryName(file.Path)))
-    //        {
-    //            continue;
-    //        }
-    //        var filename = Path.GetFileName(file.Path);
-    //        if (!MimeTypes.TryGetMimeType(filename, out var mimeType))
-    //            mimeType = MediaTypeNames.Application.Octet;
-    //        var uploadedFile = await _fileManagementClient.UploadAsync(file.FileStream, mimeType, filename);
-    //        resultFiles.Add(new GithubFile()
-    //        {
-    //            File = uploadedFile,
-    //            FilePath = file.Path
-    //        });
-    //    }
-    //    return new GetRepositoryFilesFromFilepathsResponse { Files = resultFiles };
-    //}
+    [Action("Get all files in folder", Description = "Get all files in folder")]
+    public async Task<GetRepositoryFilesFromFilepathsResponse> GetAllFilesInFolder(
+        [ActionParameter] GetRepositoryRequest repositoryRequest,
+        [ActionParameter] GetOptionalBranchRequest branchRequest,
+        [ActionParameter] FolderContentRequest folderContentRequest)
+    {
+        var projectId = (ProjectId)int.Parse(repositoryRequest.RepositoryId);
+        var resultFiles = new List<GithubFile>();
+        var content = await GetArchive(projectId, branchRequest.Name);
+        if (content == null || content.Length == 0)
+        {
+            throw new ArgumentException("Repository is empty!");
+        }
+
+        var filesFromZip = new List<BlackbirdZipEntry>();
+        using (var stream = new MemoryStream(content))
+        {
+            filesFromZip = (await stream.GetFilesFromZip()).ToList();
+        }
+        foreach (var file in filesFromZip)
+        {
+            file.Path = file.Path.Substring(file.Path.IndexOf('/') + 1);
+            var includeSubFolders = folderContentRequest.IncludeSubfolders.HasValue && folderContentRequest.IncludeSubfolders.Value;
+            if (file.FileStream.Length == 0)
+            {
+                continue;
+            }
+            else if (!string.IsNullOrEmpty(folderContentRequest.Path))
+            {
+                if ((includeSubFolders && !file.Path.StartsWith(folderContentRequest.Path)) ||
+                    (!includeSubFolders && Path.GetDirectoryName(file.Path).TrimStart('\\').Replace('\\', '/') != folderContentRequest.Path.Trim('/')))
+                {
+                    continue;
+                }
+            }
+            else if (!includeSubFolders && !string.IsNullOrEmpty(Path.GetDirectoryName(file.Path)))
+            {
+                continue;
+            }
+            var filename = Path.GetFileName(file.Path);
+            if (!MimeTypes.TryGetMimeType(filename, out var mimeType))
+                mimeType = MediaTypeNames.Application.Octet;
+            var uploadedFile = await _fileManagementClient.UploadAsync(file.FileStream, mimeType, filename);
+            resultFiles.Add(new GithubFile()
+            {
+                File = uploadedFile,
+                FilePath = file.Path
+            });
+        }
+        return new GetRepositoryFilesFromFilepathsResponse { Files = resultFiles };
+    }
 
     [Action("Get repository", Description = "Get repository info")]
     public async Task<Project> GetRepositoryById([ActionParameter] GetRepositoryRequest input)
@@ -266,8 +270,23 @@ public class RepositoryActions : GitLabActions
     //        IsFileInFolder = input.FilePath.Split('/').SkipLast(1).Contains(input.FolderName) ? 1 : 0
     //    };
     //}
+
+    private async Task<byte[]> GetArchive(ProjectId projectId, string? branchName)
+    {
+        var commits = await Client.Commits.GetAsync(projectId, 
+            (CommitQueryOptions options) => 
+            {
+                options.All = true;
+                options.RefName = branchName;
+            });
+        var branchCommit = string.IsNullOrWhiteSpace(branchName) ? $"?sha={commits.OrderBy(x => x.CreatedAt).First().Id}" : "";
+        var request = new RestRequest($"/v4/projects/{projectId}/repository/archive.zip{branchCommit}", Method.Get);
+        request.AddHeader("Authorization", $"Bearer {InvocationContext.AuthenticationCredentialsProviders.First(p => p.KeyName == "Authorization").Value}");
+        var result = await new RestClient("https://gitlab.com/api").ExecuteAsync(request);
+        return result.RawBytes;
+    }
     public static void GetRepositorySearchOptions(ProjectQueryOptions options)
     {
         options.IsMemberOf = true;
-    }
+    }    
 }
