56183a186f03082ec1ec773997b0860df451109b
bohdanm-bb <bohdan.makarenko@blackbird.io>
2024-04-18 19:01:27 +0300

    Remove bb token

--- Apps.GitLab/Webhooks/Bridge/BridgeService.cs
+++ Apps.GitLab/Webhooks/Bridge/BridgeService.cs
@@ -15,7 +15,7 @@ public class BridgeService
     {
         var client = new RestClient(BridgeServiceUrl);
         var request = new RestRequest($"/{repositoryId}/{_event}", Method.Post);
-        request.AddHeader("Blackbird-Token", ApplicationConstants.BlackbirdToken);
+        //request.AddHeader("Blackbird-Token", ApplicationConstants.BlackbirdToken);
         request.AddBody(url);
         client.Execute(request);
     }
@@ -24,12 +24,12 @@ public class BridgeService
     {
         var client = new RestClient(BridgeServiceUrl);
         var requestGet = new RestRequest($"/{repositoryId}/{_event}", Method.Get);
-        requestGet.AddHeader("Blackbird-Token", ApplicationConstants.BlackbirdToken);
+        //requestGet.AddHeader("Blackbird-Token", ApplicationConstants.BlackbirdToken);
         var webhooks = client.Get<List<BridgeGetResponse>>(requestGet);
         var webhook = webhooks.FirstOrDefault(w => w.Value == url);
 
         var requestDelete = new RestRequest($"/{repositoryId}/{_event}/{webhook.Id}", Method.Delete);
-        requestDelete.AddHeader("Blackbird-Token", ApplicationConstants.BlackbirdToken);
+        //requestDelete.AddHeader("Blackbird-Token", ApplicationConstants.BlackbirdToken);
         client.Delete(requestDelete);
     }
 }
