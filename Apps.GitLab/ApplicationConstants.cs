ff80397f1c0ab985a0f4067e0e1b5de47aa9cc03
bohdanm-bb <bohdan.makarenko@blackbird.io>
2024-05-07 17:12:46 +0300

    OAuth changes

--- Apps.GitLab/ApplicationConstants.cs
+++ Apps.GitLab/ApplicationConstants.cs
@@ -8,13 +8,11 @@ namespace Apps.Gitlab
 {
     public class ApplicationConstants
     {
-        public const string ClientId = "#{APP_GITLAB_CLIENT_ID}#";
+        public const string ClientId = "#{APP-GITLAB-CLIENT-ID}#";
 
-        //public const string ClientSecret = "#{APP_GITLAB_SECRET}#";
+        public const string ClientSecret = "#{APP-GITLAB-SECRET}#";
 
-        public const string Scope = "api read_user";//"#{APP_GITLAB_SCOPE}#";
-
-        //public const string BlackbirdToken = "#{APP_GITLAB_BLACKBIRD_TOKEN}#"; // bridge validates this token
+        public const string Scope = "#{APP_GITLAB_SCOPE}#";
 
     }
 }
