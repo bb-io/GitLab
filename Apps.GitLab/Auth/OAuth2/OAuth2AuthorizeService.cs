f72c5f173cfc217eb306b381c745683835c01adf
Artem Riabushenko <ariabushenko@blackbird.io>
2026-03-11 19:49:26 +0200

    Added new connection type oauth self-managed

--- Apps.GitLab/Auth/OAuth2/OAuth2AuthorizeService.cs
+++ Apps.GitLab/Auth/OAuth2/OAuth2AuthorizeService.cs
@@ -1,5 +1,7 @@
-﻿using Blackbird.Applications.Sdk.Common;
+﻿using Apps.GitLab.Constants;
+using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Authentication.OAuth2;
+using Blackbird.Applications.Sdk.Common.Connections;
 using Blackbird.Applications.Sdk.Common.Invocation;
 using Microsoft.AspNetCore.WebUtilities;
 
@@ -14,17 +16,51 @@ public class OAuth2AuthorizeService : BaseInvocable, IOAuth2AuthorizeService
     public string GetAuthorizationUrl(Dictionary<string, string> values)
     {
         string bridgeOauthUrl = $"{InvocationContext.UriInfo.BridgeServiceUrl.ToString().TrimEnd('/')}/oauth";
-        const string oauthUrl = "https://gitlab.com/oauth/authorize";
+
+        var connectionType = GetConnectionType(values);
+        var authorizationUrl = GetAuthorizationEndpoint(values, connectionType);
+        var clientId = GetClientId(values, connectionType);
+
         var parameters = new Dictionary<string, string>
         {
-            { "client_id", ApplicationConstants.ClientId },
+            { "client_id", clientId },
             { "redirect_uri", $"{InvocationContext.UriInfo.BridgeServiceUrl.ToString().TrimEnd('/')}/AuthorizationCode" },
             { "scope", ApplicationConstants.Scope },
             { "state", values["state"] },
             { "response_type", "code" },
-            { "authorization_url", oauthUrl},
+            { "authorization_url", authorizationUrl },
             { "actual_redirect_uri", InvocationContext.UriInfo.AuthorizationCodeRedirectUri.ToString() },
         };
+
         return QueryHelpers.AddQueryString(bridgeOauthUrl, parameters);
+    }
+
+    private static string GetConnectionType(Dictionary<string, string> values)
+    {
+        var connectionType = values[nameof(ConnectionPropertyGroup)];
+
+        return ConnectionTypes.SupportedConnectionTypes.Contains(connectionType)
+            ? connectionType
+            : throw new Exception($"Unsupported connection type: {connectionType}");
+    }
+
+    private static string GetAuthorizationEndpoint(Dictionary<string, string> values, string connectionType)
+    {
+        return connectionType switch
+        {
+            ConnectionTypes.OAuth => "https://gitlab.com/oauth/authorize",
+            ConnectionTypes.OAuthSelfManaged => $"{values[CredNames.BaseUrl].TrimEnd('/')}/oauth/authorize",
+            _ => throw new Exception($"Unsupported connection type for OAuth authorization: {connectionType}")
+        };
+    }
+
+    private static string GetClientId(Dictionary<string, string> values, string connectionType)
+    {
+        return connectionType switch
+        {
+            ConnectionTypes.OAuth => ApplicationConstants.ClientId,
+            ConnectionTypes.OAuthSelfManaged => values[CredNames.ClientId],
+            _ => throw new Exception($"Unsupported connection type for OAuth authorization: {connectionType}")
+        };
     }
 }
